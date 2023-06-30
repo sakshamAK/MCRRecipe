6e8cd922022034feba84416847b2f5784c96fee9
sakshamak <saksham.8484@gmail.com>
2023-06-30 10:30:03 +0530

    feat: added labels

--- src/pages/components/RecipeForm/RecipeForm.jsx
+++ src/pages/components/RecipeForm/RecipeForm.jsx
@@ -9,9 +9,18 @@ export const RecipeForm = () => {
     return (
         <form className={style["recipe-form"]} style={{ display: toggle ? "flex" : "none" }} ref={formRef}>
             <h3>{modifySingleRecipe ? "Modify Recipe" : "Add New Recipe"}</h3>
-            <input name="name" type="text" placeholder="Name of recipe" value={formValues.name} onChange={e => setRecipeValues(e, setFormValues)} />
-            <input name="cuisine" type="text" placeholder="Cuisine type" value={formValues.cuisine} onChange={e => setRecipeValues(e, setFormValues)} />
-            <input name="ingredients" type="text" placeholder="Ingredients of Recipe" value={formValues.ingredients} onChange={e => setRecipeValues(e, setFormValues)} />
+            <div className={style["form-input"]}>
+                <label>Name of recipe: </label>
+                <input name="name" type="text" placeholder="Name of recipe" value={formValues.name} onChange={e => setRecipeValues(e, setFormValues)} />
+            </div>
+            <div className={style["form-input"]}>
+                <label>Cuisine type: </label>
+                <input name="cuisine" type="text" placeholder="Cuisine type" value={formValues.cuisine} onChange={e => setRecipeValues(e, setFormValues)} />
+            </div>
+            <div className={style["form-input"]}>
+                <label>Ingredients of Recipe: </label>
+                <input name="ingredients" type="text" placeholder="Ingredients of Recipe" value={formValues.ingredients} onChange={e => setRecipeValues(e, setFormValues)} />
+            </div>
             <div>
                 {modifySingleRecipe ? <input type="submit" value="Modify Recipe" style={{ marginBlock: "1rem" }} onClick={(e) => editRecipe(e, modifySingleRecipe, formRef, formValues, recipe, setRecipe, setToggle)} /> :
                     <input type="submit" value="Add Recipe" style={{ marginBlock: "1rem" }} onClick={(e) => submitRecipe(e, formRef, setRecipe, setToggle)} />}
